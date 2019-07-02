f28c5a279f5ef27cd293e2477bd88de4b67cdf15
James Ross <jross@palantir.com>
2019-07-02 14:33:56 -0700

    Add periodic.Option type for running initial poll before waiting.

---
+++ status/health/periodic/option.go
@@ -0,0 +1,17 @@
+package periodic
+
+type Option interface {
+	apply(source *healthCheckSource)
+}
+
+type optionFn func(source *healthCheckSource)
+
+func(fn optionFn) apply(source *healthCheckSource) {
+	fn(source)
+}
+
+func WithInitialPoll() Option {
+	return optionFn(func(source *healthCheckSource) {
+		source.initialPoll = true
+	})
+}
