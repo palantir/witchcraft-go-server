3471451b2e083a5dc2ea84223c55718604f5c896
Lucas França de Oliveira <lucasfdo@palantir.com>
2019-09-25 20:44:03 +0100

    fixed typo

--- status/health/window/source.go
+++ status/health/window/source.go
@@ -24,20 +24,20 @@ import (
 // ErrorsToCheckFn is a function that constructs a HealthCheckResult from a set of errors.
 type ErrorsToCheckFn func(ctx context.Context, errors []error) health.HealthCheckResult
 
-type healchCheckSource struct {
+type healthCheckSource struct {
 	slidingWindowManager *SlidingWindowManager
 	errorsToCheckFn      ErrorsToCheckFn
 }
 
 // NewHealthCheck creates a HealthCheckSource that polls a SlidingWindowManager and returns a HealthStatus created using an ErrorsToCheckFn.
 func NewHealthCheck(slidingWindowManager *SlidingWindowManager, errorsToCheckFn ErrorsToCheckFn) status.HealthCheckSource {
-	return &healchCheckSource{
+	return &healthCheckSource{
 		slidingWindowManager: slidingWindowManager,
 		errorsToCheckFn:      errorsToCheckFn,
 	}
 }
 
-func (h *healchCheckSource) HealthStatus(ctx context.Context) health.HealthStatus {
+func (h *healthCheckSource) HealthStatus(ctx context.Context) health.HealthStatus {
 	checkResult := h.errorsToCheckFn(ctx, h.slidingWindowManager.GetErrors())
 	return health.HealthStatus{
 		Checks: map[health.CheckType]health.HealthCheckResult{
