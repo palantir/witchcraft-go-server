8de6c199905cd9d698b7ed9c55623e703c8d9937
Lucas França de Oliveira <lucasfdo@palantir.com>
2019-09-27 20:45:06 +0100

    changed tests to use require

--- status/health/window/window_event_storer_test.go
+++ status/health/window/window_event_storer_test.go
@@ -18,56 +18,56 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTimeWindowedEventStorer_ErrorOnCreate(t *testing.T) {
 	_, err := NewTimeWindowedEventStorer(0)
 	// This should error as 0 is not a valid windowSize.
-	assert.Error(t, err)
+	require.Error(t, err)
 }
 
 func TestTimeWindowedEventStorer_NoEvents(t *testing.T) {
 	manager, err := NewTimeWindowedEventStorer(time.Millisecond)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	items := manager.GetItemsInWindow()
-	assert.Nil(t, items)
+	require.Nil(t, items)
 }
 
 func TestTimeWindowedEventStorer_AllEventsUpToDate(t *testing.T) {
 	manager, err := NewTimeWindowedEventStorer(time.Second)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	manager.Submit("payload #1")
 	manager.Submit("payload #2")
 	manager.Submit("payload #3")
 	items := manager.GetItemsInWindow()
-	assert.EqualValues(t, len(items), 3)
-	assert.EqualValues(t, "payload #1", items[0].Payload)
-	assert.EqualValues(t, "payload #2", items[1].Payload)
-	assert.EqualValues(t, "payload #3", items[2].Payload)
+	require.EqualValues(t, len(items), 3)
+	require.EqualValues(t, "payload #1", items[0].Payload)
+	require.EqualValues(t, "payload #2", items[1].Payload)
+	require.EqualValues(t, "payload #3", items[2].Payload)
 }
 
 func TestTimeWindowedEventStorer_AllEventsOutOfDate(t *testing.T) {
 	manager, err := NewTimeWindowedEventStorer(50 * time.Millisecond)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	manager.Submit("payload #1")
 	manager.Submit("payload #2")
 	manager.Submit("payload #3")
 	<-time.After(100 * time.Millisecond)
 	items := manager.GetItemsInWindow()
-	assert.Empty(t, items)
+	require.Empty(t, items)
 }
 
 func TestTimeWindowedEventStorer_SomeEventsOutOfDate(t *testing.T) {
 	manager, err := NewTimeWindowedEventStorer(500 * time.Millisecond)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	manager.Submit("payload #1")
 	manager.Submit("payload #2")
 	<-time.After(time.Second)
 	manager.Submit("payload #3")
 	manager.Submit("payload #4")
 	items := manager.GetItemsInWindow()
-	assert.EqualValues(t, len(items), 2)
-	assert.EqualValues(t, "payload #3", items[0].Payload)
-	assert.EqualValues(t, "payload #4", items[1].Payload)
+	require.EqualValues(t, len(items), 2)
+	require.EqualValues(t, "payload #3", items[0].Payload)
+	require.EqualValues(t, "payload #4", items[1].Payload)
 }
