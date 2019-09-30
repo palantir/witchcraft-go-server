6c14d30306e211ce36758271eeabff166197c653
James Ross <jross@palantir.com>
2019-09-30 16:39:10 -0700

    Update log message for invoking server initialization function.

--- witchcraft/witchcraft.go
+++ witchcraft/witchcraft.go
@@ -560,7 +560,7 @@ func (s *Server) Start() (rErr error) {
 		}
 		ctx = wtracing.ContextWithTracer(ctx, tracer)
 
-		svc1log.FromContext(ctx).Debug("Invoking server init func.")
+		svc1log.FromContext(ctx).Debug("Running server initialization function.")
 		cleanupFn, err := s.initFn(
 			ctx,
 			InitInfo{
