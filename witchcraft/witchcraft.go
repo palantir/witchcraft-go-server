172c773b79bc8bb02c386041f460f5448382b5f6
James Ross <jross@palantir.com>
2019-09-30 16:17:33 -0700

    Change init func invocation log line to level DEBUG.

--- witchcraft/witchcraft.go
+++ witchcraft/witchcraft.go
@@ -560,7 +560,7 @@ func (s *Server) Start() (rErr error) {
 		}
 		ctx = wtracing.ContextWithTracer(ctx, tracer)
 
-		svc1log.FromContext(ctx).Info("Invoking server init func.")
+		svc1log.FromContext(ctx).Debug("Invoking server init func.")
 		cleanupFn, err := s.initFn(
 			ctx,
 			InitInfo{
