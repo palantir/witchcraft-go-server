b19aa99bd735a80a35467366b8be1116befde1c4
Nick Miyake <nmiyake@users.noreply.github.com>
2021-03-26 10:56:04 -0700

    Log metrics that go from 0 to 1 (#274)

--- witchcraft/server_metrics.go
+++ witchcraft/server_metrics.go
@@ -16,9 +16,14 @@ package witchcraft
 
 import (
 	"context"
+	"fmt"
+	"reflect"
 	"runtime"
+	"sort"
+	"sync"
 	"time"
 
+	gometrics "github.com/palantir/go-metrics"
 	"github.com/palantir/pkg/metrics"
 	werror "github.com/palantir/witchcraft-go-error"
 	"github.com/palantir/witchcraft-go-logging/wlog/metriclog/metric1log"
@@ -57,6 +62,14 @@ var (
 	initTime = time.Now()
 )
 
+// seenMetricsSet pairs a set with a lock that can be used to control access to the set.
+type seenMetricsSet struct {
+	// protects access to seenSet
+	lock sync.Mutex
+	// records metrics that have been logged
+	seenSet map[string]struct{}
+}
+
 func (s *Server) initMetrics(ctx context.Context, installCfg config.Install) (rRegistry metrics.RootRegistry, rDeferFn func(), rErr error) {
 	metricsRegistry := metrics.DefaultMetricsRegistry
 	metricsEmitFreq := defaultMetricEmitFrequency
@@ -78,6 +91,14 @@ func (s *Server) initMetrics(ctx context.Context, installCfg config.Install) (rR
 		metricTypeValuesBlacklist = defaultMetricTypeValuesBlacklist()
 	}
 
+	// seenMetrics tracks the metrics that have been seen so far. Uses a lock to protect access because emitFn that
+	// reads and writes this map can be called in the "emit" goroutine and in the goroutine that runs the rDeferFn
+	// returned by this function.
+	seenMetrics := &seenMetricsSet{
+		// keys should be created by tagMapKey function
+		seenSet: make(map[string]struct{}),
+	}
+
 	emitFn := func(metricID string, tags metrics.Tags, metricVal metrics.MetricVal) {
 		if _, blackListed := s.metricsBlacklist[metricID]; blackListed {
 			// skip emitting metric if it is blacklisted
@@ -86,12 +107,7 @@ func (s *Server) initMetrics(ctx context.Context, installCfg config.Install) (rR
 
 		valuesToUse := metricVal.Values()
 		metricType := metricVal.Type()
-		if metricTypeValueBlacklist, ok := metricTypeValuesBlacklist[metricType]; ok {
-			// remove blacklisted keys
-			for blacklistedKey := range metricTypeValueBlacklist {
-				delete(valuesToUse, blacklistedKey)
-			}
-		}
+		removeDisallowedKeys(metricType, valuesToUse, metricTypeValuesBlacklist)
 		if len(valuesToUse) == 0 {
 			// do not record metric if it does not have any values
 			return
@@ -104,7 +120,20 @@ func (s *Server) initMetrics(ctx context.Context, installCfg config.Install) (rR
 		if localMetricLogger == nil {
 			return
 		}
-		localMetricLogger.Metric(metricID, metricType, metric1log.Values(valuesToUse), metric1log.Tags(tags.ToMap()))
+
+		tagsMap := tags.ToMap()
+		metricTagsParam := metric1log.Tags(tagsMap)
+
+		// if metric is one for which a zero value should be logged on first observation, log a zero value if necessary
+		if isZeroValueMetric(metricType) {
+			zeroValuesLogged := logZeroValueMetric(localMetricLogger, metricID, metricType, tagsMap, seenMetrics, metricTypeValuesBlacklist, metricTagsParam)
+
+			// if the zeroValueLogged is equivalent to valuesToUse, then there's no need to log the metric again
+			if zeroValuesLogged != nil && reflect.DeepEqual(zeroValuesLogged, valuesToUse) {
+				return
+			}
+		}
+		localMetricLogger.Metric(metricID, metricType, metric1log.Values(valuesToUse), metricTagsParam)
 	}
 
 	// start goroutine that logs metrics at the given frequency
@@ -116,6 +145,101 @@ func (s *Server) initMetrics(ctx context.Context, installCfg config.Install) (rR
 		// emit all metrics a final time on termination
 		metricsRegistry.Each(emitFn)
 	}, nil
+}
+
+// isZeroValueMetric returns true if the provided metric type is a type of metric for which a zero value should be
+// emitted before a regular value is emitted. This is true for metrics for which analysis often requires determining
+// when the value has changed: for such metrics, if a value for a given metric, type, and set of tags has not been
+// emitted before, a zero-value version of the metric is emitted before the regular metric is emitted to ensure that a
+// value change can be observed. Currently, this is done for "meter", "timer" and "histogram" types.
+func isZeroValueMetric(metricType string) bool {
+	return metricType == "meter" || metricType == "timer" || metricType == "histogram"
+}
+
+// zeroValuesForMetricType returns the metric values for the "zero" value of the metric of the given type. The provided
+// metric type must be a valid type that returns true when provided to the "isZeroValueMetric" function: panics
+// otherwise.
+func zeroValuesForMetricType(metricType string) map[string]interface{} {
+	var zeroMetric interface{}
+	switch metricType {
+	case "meter":
+		zeroMetric = gometrics.NewMeter()
+	case "timer":
+		zeroMetric = gometrics.NewTimer()
+	case "histogram":
+		zeroMetric = gometrics.NewHistogram(metrics.DefaultSample())
+	default:
+		panic(fmt.Sprintf("should never happen: unsupported metricType %s", metricType))
+	}
+	return metrics.ToMetricVal(zeroMetric).Values()
+}
+
+// logZeroValueMetric logs a "zero" value for metric with the provided metricID, metricType, and tags to the provided
+// metricLogger if an entry for the metric has not yet been logged (which is determined based on whether or not an entry
+// for the metric exists in the provided seenMetrics set). Returns the zero values of the logged metric. If a zero value
+// is logged, an entry for the metric is added to seenMetrics.
+func logZeroValueMetric(
+	metricLogger metric1log.Logger,
+	metricID,
+	metricType string,
+	tags map[string]string,
+	seenMetrics *seenMetricsSet,
+	metricTypeValuesDisallowedList map[string]map[string]struct{},
+	metricTagsParam metric1log.Param,
+) (zeroValuesLogged map[string]interface{}) {
+
+	// Acquire lock to ensure that map access is safe. Safe to lock for the entirety of the function rather than
+	// selectively locking just the read and write of the map because this function will be called in a single goroutine
+	// in all instances except when the server shuts down.
+	seenMetrics.lock.Lock()
+	defer seenMetrics.lock.Unlock()
+
+	// if metric has been seen before, no need to log zero value
+	mapKey := tagMapKey(metricID, metricType, tags)
+
+	_, metricSeen := seenMetrics.seenSet[mapKey]
+	if metricSeen {
+		return nil
+	}
+
+	zeroValuesToUse := zeroValuesForMetricType(metricType)
+	removeDisallowedKeys(metricType, zeroValuesToUse, metricTypeValuesDisallowedList)
+
+	// if removing the disallowed keys causes the values to be empty, no need to log zero value
+	if len(zeroValuesToUse) == 0 {
+		return nil
+	}
+
+	// metric not seen before: emit zero-value and record
+	metricLogger.Metric(metricID, metricType, metric1log.Values(zeroValuesToUse), metricTagsParam)
+	seenMetrics.seenSet[mapKey] = struct{}{}
+	return zeroValuesToUse
+}
+
+// tagMapKey returns a string that provides a unique key for the provided metricID, metricType and tagsMap. The content
+// of the map is put into a slice where each element is a string of the form "{key}:{value}" and the slice is then
+// sorted. The tags map (rather than slice) is used to construct the key because the map is what the metric logger
+// ultimately emits.
+func tagMapKey(metricID, metricType string, tagsMap map[string]string) string {
+	tagsSlice := make([]string, len(tagsMap))
+	idx := 0
+	for k, v := range tagsMap {
+		tagsSlice[idx] = fmt.Sprintf("%s:%s", k, v)
+		idx++
+	}
+	sort.Strings(tagsSlice)
+	return fmt.Sprintf("%s:%s:%v", metricID, metricType, tagsSlice)
+}
+
+func removeDisallowedKeys(metricType string, metricVals map[string]interface{}, disallowedKeys map[string]map[string]struct{}) {
+	disallowedKeysForType, ok := disallowedKeys[metricType]
+	if !ok {
+		return
+	}
+	// remove disallowed keys
+	for blacklistedKey := range disallowedKeysForType {
+		delete(metricVals, blacklistedKey)
+	}
 }
 
 func initServerUptimeMetric(ctx context.Context, metricsRegistry metrics.Registry) {
