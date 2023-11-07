604a37e7addeb5ab5323dacc3689f25926b0b263
Adam Anderson <adamanderson@palantir.com>
2023-11-06 16:45:31 -0800

    more diagnostic handlers

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/palantir/witchcraft-go-server/v2
 
-go 1.20
+go 1.21
+
+toolchain go1.21.1
 
 require (
 	github.com/gorilla/mux v1.7.3
--- witchcraft/internal/wdebug/handlers.go
+++ witchcraft/internal/wdebug/handlers.go
@@ -16,8 +16,14 @@ package wdebug
 
 import (
 	"context"
+	"fmt"
 	"io"
+	"net/http"
+	"net/http/httptest"
+	netpprof "net/http/pprof"
+	"runtime"
 	"runtime/pprof"
+	"runtime/trace"
 	"time"
 
 	"github.com/palantir/conjure-go-runtime/v2/conjure-go-contract/codecs"
@@ -28,21 +34,27 @@ import (
 )
 
 const (
-	DiagnosticTypeCPUProfile1MinuteV1 DiagnosticType = "go.profile.cpu.1minute.v1"
-	DiagnosticTypeHeapProfileV1       DiagnosticType = "go.profile.heap.v1"
-	DiagnosticTypeAllocsProfileV1     DiagnosticType = "go.profile.allocs.v1"
-	DiagnosticTypeGoroutinesV1        DiagnosticType = "go.goroutines.v1"
-	DiagnosticTypeMetricNamesV1       DiagnosticType = "metric.names.v1"
-	DiagnosticTypeSystemTimeV1        DiagnosticType = "os.system.clock.v1"
+	DiagnosticTypeCPUProfile1MinuteV1   DiagnosticType = "go.profile.cpu.1minute.v1"
+	DiagnosticTypeHeapProfileV1         DiagnosticType = "go.profile.heap.v1"
+	DiagnosticTypeAllocsProfileV1       DiagnosticType = "go.profile.allocs.v1"
+	DiagnosticTypeBlockProfile1MinuteV1 DiagnosticType = "go.profile.block.1minute.v1"
+	DiagnosticTypeMutexProfile1MinuteV1 DiagnosticType = "go.profile.mutex.1minute.v1"
+	DiagnosticTypeTrace1MinuteV1        DiagnosticType = "go.trace.1minute.v1"
+	DiagnosticTypeGoroutinesV1          DiagnosticType = "go.goroutines.v1"
+	DiagnosticTypeMetricNamesV1         DiagnosticType = "metric.names.v1"
+	DiagnosticTypeSystemTimeV1          DiagnosticType = "os.system.clock.v1"
 )
 
 var diagnosticHandlers = map[DiagnosticType]DiagnosticHandler{
-	DiagnosticTypeCPUProfile1MinuteV1: handlerCPUProfile1MinuteV1{},
-	DiagnosticTypeHeapProfileV1:       handlerHeapProfileV1{},
-	DiagnosticTypeAllocsProfileV1:     handlerAllocsProfileV1{},
-	DiagnosticTypeGoroutinesV1:        handlerGoroutinesV1{},
-	DiagnosticTypeMetricNamesV1:       handlerMetricNamesV1{},
-	DiagnosticTypeSystemTimeV1:        handlerSystemTimeV1{},
+	DiagnosticTypeCPUProfile1MinuteV1:   handlerCPUProfile1MinuteV1{},
+	DiagnosticTypeHeapProfileV1:         handlerHeapProfileV1{},
+	DiagnosticTypeAllocsProfileV1:       handlerAllocsProfileV1{},
+	DiagnosticTypeBlockProfile1MinuteV1: handlerBlockProfile1MinuteV1{},
+	DiagnosticTypeMutexProfile1MinuteV1: handlerMutexProfile1MinuteV1{},
+	DiagnosticTypeTrace1MinuteV1:        handlerTrace1MinuteV1{},
+	DiagnosticTypeGoroutinesV1:          handlerGoroutinesV1{},
+	DiagnosticTypeMetricNamesV1:         handlerMetricNamesV1{},
+	DiagnosticTypeSystemTimeV1:          handlerSystemTimeV1{},
 }
 
 type DiagnosticHandler interface {
@@ -181,6 +193,113 @@ func (h handlerAllocsProfileV1) WriteDiagnostic(ctx context.Context, w io.Writer
 	return nil
 }
 
+type handlerBlockProfile1MinuteV1 struct{}
+
+func (h handlerBlockProfile1MinuteV1) Type() DiagnosticType {
+	return DiagnosticTypeBlockProfile1MinuteV1
+}
+
+func (h handlerBlockProfile1MinuteV1) ContentType() string {
+	return codecs.Binary.ContentType()
+}
+
+func (h handlerBlockProfile1MinuteV1) Documentation() string {
+	return `A profile recording traces where the program was blocked on synchronization primitives. See golang docs for analysis tooling: https://golang.org/doc/diagnostics.html#profiling`
+}
+
+func (h handlerBlockProfile1MinuteV1) SafeLoggable() bool {
+	return true
+}
+
+func (h handlerBlockProfile1MinuteV1) Extension() string {
+	return "prof"
+}
+
+func (h handlerBlockProfile1MinuteV1) WriteDiagnostic(ctx context.Context, w io.Writer) error {
+	const duration = time.Minute
+	// More info here: https://github.com/DataDog/go-profiler-notes/blob/main/guide/README.md#block-profiler
+	const profileRate = 10 * time.Microsecond
+
+	runtime.SetBlockProfileRate(int(profileRate.Nanoseconds()))
+	defer runtime.SetBlockProfileRate(0)
+
+	return takeDeltaProfile(ctx, w, "block", duration)
+}
+
+type handlerMutexProfile1MinuteV1 struct{}
+
+func (h handlerMutexProfile1MinuteV1) Type() DiagnosticType {
+	return DiagnosticTypeMutexProfile1MinuteV1
+}
+
+func (h handlerMutexProfile1MinuteV1) ContentType() string {
+	return codecs.Binary.ContentType()
+}
+
+func (h handlerMutexProfile1MinuteV1) Documentation() string {
+	return `A profile recording traces of contending mutexes. See golang docs for analysis tooling: https://golang.org/doc/diagnostics.html#profiling`
+}
+
+func (h handlerMutexProfile1MinuteV1) SafeLoggable() bool {
+	return true
+}
+
+func (h handlerMutexProfile1MinuteV1) Extension() string {
+	return "prof"
+}
+
+func (h handlerMutexProfile1MinuteV1) WriteDiagnostic(ctx context.Context, w io.Writer) error {
+	const duration = time.Minute
+	// More info here: https://github.com/DataDog/go-profiler-notes/blob/main/guide/README.md#mutex-profiler
+	// Google uses 100 internally: https://github.com/golang/go/issues/23401#issuecomment-367029643
+	const profileFraction = 100
+
+	runtime.SetMutexProfileFraction(profileFraction)
+	defer runtime.SetMutexProfileFraction(0)
+
+	return takeDeltaProfile(ctx, w, "mutex", duration)
+}
+
+type handlerTrace1MinuteV1 struct{}
+
+func (h handlerTrace1MinuteV1) Type() DiagnosticType {
+	return DiagnosticTypeTrace1MinuteV1
+}
+
+func (h handlerTrace1MinuteV1) ContentType() string {
+	return codecs.Binary.ContentType()
+}
+
+func (h handlerTrace1MinuteV1) Documentation() string {
+	return `An execution trace of the program for 1 minute. See golang docs for analysis tooling: https://golang.org/doc/diagnostics.html#profiling`
+}
+
+func (h handlerTrace1MinuteV1) SafeLoggable() bool {
+	return true
+}
+
+func (h handlerTrace1MinuteV1) Extension() string {
+	return "prof"
+}
+
+func (h handlerTrace1MinuteV1) WriteDiagnostic(ctx context.Context, w io.Writer) error {
+	const duration = time.Minute
+
+	if err := trace.Start(w); err != nil {
+		err = werror.WrapWithContextParams(ctx, err, "failed to start execution tracer")
+		return errors.WrapWithConflict(err, wparams.NewSafeParamStorer(map[string]interface{}{
+			"message": err.Error(),
+		}))
+	}
+	defer trace.Stop()
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-time.After(duration):
+	}
+	return nil
+}
+
 type handlerMetricNamesV1 struct{}
 
 func (h handlerMetricNamesV1) Type() DiagnosticType {
@@ -246,6 +365,36 @@ func (h handlerSystemTimeV1) Extension() string {
 func (h handlerSystemTimeV1) WriteDiagnostic(ctx context.Context, w io.Writer) error {
 	if err := codecs.Plain.Encode(w, time.Now().Format(time.RFC3339Nano)); err != nil {
 		return werror.WrapWithContextParams(ctx, err, "failed to write system time")
+	}
+	return nil
+}
+
+func takeDeltaProfile(ctx context.Context, w io.Writer, pprofHandler string, duration time.Duration) error {
+	// The mechanics of taking a delta profile (i.e. diff-ing two profiles)
+	// are quite complicated and depends on the banned "internal" package
+	// See https://github.com/golang/go/issues/23401 for more info
+	//
+	// The netpprof package includes the necessary logic for taking a correct
+	// block profile, but is locked behind HTTP semantics :(
+	//
+	// We work around that by making a dummy request directly to the appropriate handler
+
+	fakeURL := fmt.Sprintf("http://localhost/debug/%s/block?seconds=%d", pprofHandler, int(duration.Seconds()))
+	req, err := http.NewRequestWithContext(ctx, "GET", fakeURL, nil)
+	if err != nil {
+		err = werror.WrapWithContextParams(ctx, err, "failed to create dummy request to netpprof handler")
+		return errors.WrapWithInternal(err)
+	}
+	rec := httptest.NewRecorder()
+	netpprof.Handler(pprofHandler).ServeHTTP(rec, req)
+	resp := rec.Result()
+	if resp.StatusCode != http.StatusOK {
+		err = werror.ErrorWithContextParams(ctx, "pprof handler failed to capture profile")
+		return errors.WrapWithInternal(err)
+	}
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		return werror.ErrorWithContextParams(ctx, "failed to write profile to response stream")
 	}
 	return nil
 }
