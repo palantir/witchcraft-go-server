89404d04c5652cb1b89c8c6aa7047c6d2e66bc12
Brad Moylan <bmoylan@palantir.com>
2025-03-27 12:09:59 -0700

    improvement: Avoid computing unused metric values

--- witchcraft/server_metrics.go
+++ witchcraft/server_metrics.go
@@ -19,7 +19,7 @@ import (
 	"fmt"
 	"reflect"
 	"runtime"
-	"sort"
+	"slices"
 	"sync"
 	"time"
 
@@ -87,6 +87,9 @@ func (s *Server) initMetrics(ctx context.Context, installCfg config.Install) (rR
 		}
 	}
 
+	if s.metricsBlacklist == nil {
+		s.metricsBlacklist = make(map[string]struct{})
+	}
 	if s.metricTypeValuesBlacklist == nil {
 		s.metricTypeValuesBlacklist = defaultMetricTypeValuesBlacklist()
 	}
@@ -101,7 +104,7 @@ func (s *Server) initMetrics(ctx context.Context, installCfg config.Install) (rR
 
 	emitFn := func(metricID string, tags metrics.Tags, metricVal metrics.MetricVal) {
 		metricType := metricVal.Type()
-		valuesToUse := s.filterMetricValues(metricID, metricVal)
+		valuesToUse := s.collectMetricValues(metricID, metricVal)
 		if len(valuesToUse) == 0 {
 			// do not record metric if it does not have any values
 			return
@@ -120,13 +123,14 @@ func (s *Server) initMetrics(ctx context.Context, installCfg config.Install) (rR
 
 		// if metric is one for which a zero value should be logged on first observation, log a zero value if necessary
 		if isZeroValueMetric(metricType) {
-			zeroValuesLogged := logZeroValueMetric(localMetricLogger, metricID, metricType, tagsMap, seenMetrics, s.metricTypeValuesBlacklist, metricTagsParam)
+			zeroValuesLogged := s.logZeroValueMetric(localMetricLogger, metricID, metricType, tagsMap, seenMetrics, metricTagsParam)
 
 			// if the zeroValueLogged is equivalent to valuesToUse, then there's no need to log the metric again
 			if zeroValuesLogged != nil && reflect.DeepEqual(zeroValuesLogged, valuesToUse) {
 				return
 			}
 		}
+
 		localMetricLogger.Metric(metricID, metricType, metric1log.Values(valuesToUse), metricTagsParam)
 	}
 
@@ -141,14 +145,31 @@ func (s *Server) initMetrics(ctx context.Context, installCfg config.Install) (rR
 	}, nil
 }
 
-func (s *Server) filterMetricValues(metricID string, metricVal metrics.MetricVal) map[string]interface{} {
+func (s *Server) collectMetricValues(metricName string, metricVal metrics.MetricVal) map[string]interface{} {
+	valuesToUse := make(map[string]interface{})
+	s.visitMetricValues(metricName, metricVal, func(key string) {
+		valuesToUse[key] = metricVal.Value(key)
+	})
+	return valuesToUse
+}
+
+// visitMetricValues calls the provided visit function for each key in the provided metric value,
+// If the metric name or key is blacklisted, the visit function is not called.
+func (s *Server) visitMetricValues(metricID string, metricVal metrics.MetricVal, visit func(key string)) {
 	if _, blackListed := s.metricsBlacklist[metricID]; blackListed {
 		// skip emitting metric if it is blacklisted
-		return nil
+		return
 	}
-	valuesToUse := metricVal.Values()
-	removeDisallowedKeys(metricVal.Type(), valuesToUse, s.metricTypeValuesBlacklist)
-	return valuesToUse
+	for key := range metricVal.Keys {
+		if s.metricTypeValuesBlacklist != nil {
+			if disallowedKeysForType, ok := s.metricTypeValuesBlacklist[metricVal.Type()]; ok {
+				if _, disallowed := disallowedKeysForType[key]; disallowed {
+					continue
+				}
+			}
+		}
+		visit(key)
+	}
 }
 
 // isZeroValueMetric returns true if the provided metric type is a type of metric for which a zero value should be
@@ -163,7 +184,7 @@ func isZeroValueMetric(metricType string) bool {
 // zeroValuesForMetricType returns the metric values for the "zero" value of the metric of the given type. The provided
 // metric type must be a valid type that returns true when provided to the "isZeroValueMetric" function: panics
 // otherwise.
-func zeroValuesForMetricType(metricType string) map[string]interface{} {
+func (s *Server) zeroValuesForMetricType(metricName string, metricType string) map[string]interface{} {
 	var zeroMetric interface{}
 	switch metricType {
 	case "meter":
@@ -175,20 +196,19 @@ func zeroValuesForMetricType(metricType string) map[string]interface{} {
 	default:
 		panic(fmt.Sprintf("should never happen: unsupported metricType %s", metricType))
 	}
-	return metrics.ToMetricVal(zeroMetric).Values()
+	return s.collectMetricValues(metricName, metrics.ToMetricVal(zeroMetric))
 }
 
 // logZeroValueMetric logs a "zero" value for metric with the provided metricID, metricType, and tags to the provided
 // metricLogger if an entry for the metric has not yet been logged (which is determined based on whether or not an entry
 // for the metric exists in the provided seenMetrics set). Returns the zero values of the logged metric. If a zero value
 // is logged, an entry for the metric is added to seenMetrics.
-func logZeroValueMetric(
+func (s *Server) logZeroValueMetric(
 	metricLogger metric1log.Logger,
-	metricID,
+	metricID string,
 	metricType string,
 	tags map[string]string,
 	seenMetrics *seenMetricsSet,
-	metricTypeValuesDisallowedList map[string]map[string]struct{},
 	metricTagsParam metric1log.Param,
 ) (zeroValuesLogged map[string]interface{}) {
 
@@ -206,8 +226,7 @@ func logZeroValueMetric(
 		return nil
 	}
 
-	zeroValuesToUse := zeroValuesForMetricType(metricType)
-	removeDisallowedKeys(metricType, zeroValuesToUse, metricTypeValuesDisallowedList)
+	zeroValuesToUse := s.zeroValuesForMetricType(metricID, metricType)
 
 	// if removing the disallowed keys causes the values to be empty, no need to log zero value
 	if len(zeroValuesToUse) == 0 {
@@ -228,22 +247,11 @@ func tagMapKey(metricID, metricType string, tagsMap map[string]string) string {
 	tagsSlice := make([]string, len(tagsMap))
 	idx := 0
 	for k, v := range tagsMap {
-		tagsSlice[idx] = fmt.Sprintf("%s:%s", k, v)
+		tagsSlice[idx] = k + ":" + v
 		idx++
 	}
-	sort.Strings(tagsSlice)
+	slices.Sort(tagsSlice)
 	return fmt.Sprintf("%s:%s:%v", metricID, metricType, tagsSlice)
-}
-
-func removeDisallowedKeys(metricType string, metricVals map[string]interface{}, disallowedKeys map[string]map[string]struct{}) {
-	disallowedKeysForType, ok := disallowedKeys[metricType]
-	if !ok {
-		return
-	}
-	// remove disallowed keys
-	for blacklistedKey := range disallowedKeysForType {
-		delete(metricVals, blacklistedKey)
-	}
 }
 
 func initServerUptimeMetric(ctx context.Context, metricsRegistry metrics.Registry) {
@@ -252,7 +260,7 @@ func initServerUptimeMetric(ctx context.Context, metricsRegistry metrics.Registr
 	ctx = metrics.AddTags(ctx, metrics.MustNewTag("go_os", runtime.GOOS))
 	ctx = metrics.AddTags(ctx, metrics.MustNewTag("go_arch", runtime.GOARCH))
 
-	metrics.FromContext(ctx).Gauge("server.uptime").Update(int64(time.Since(initTime) / time.Microsecond))
+	metrics.FromContext(ctx).Gauge("server.uptime").Update(time.Since(initTime).Microseconds())
 
 	// start goroutine that updates the uptime metric
 	go wapp.RunWithRecoveryLogging(ctx, func(ctx context.Context) {
@@ -263,7 +271,7 @@ func initServerUptimeMetric(ctx context.Context, metricsRegistry metrics.Registr
 			case <-ctx.Done():
 				return
 			case <-t.C:
-				metrics.FromContext(ctx).Gauge("server.uptime").Update(int64(time.Since(initTime) / time.Microsecond))
+				metrics.FromContext(ctx).Gauge("server.uptime").Update(time.Since(initTime).Microseconds())
 			}
 		}
 	})
@@ -280,9 +288,10 @@ func (s *Server) initCardinalityMetric(ctx context.Context, metricsRegistry metr
 				return
 			case <-t.C:
 				var count int64
-				metricsRegistry.Each(func(metricID string, _ metrics.Tags, metricVal metrics.MetricVal) {
-					valuesToUse := s.filterMetricValues(metricID, metricVal)
-					count += int64(len(valuesToUse))
+				metricsRegistry.Each(func(metricName string, tags metrics.Tags, val metrics.MetricVal) {
+					s.visitMetricValues(metricName, val, func(key string) {
+						count++
+					})
 				})
 				metricsRegistry.Gauge("server.metric_cardinality").Update(count)
 			}
